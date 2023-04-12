2573df7a9be2268d1537b01041bf5363ca1f23bc
prof86k <kkumasampson@gmail.com>
2023-04-12 17:39:10 +0000

    theme of the application updated

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from '../helpers/rootRouter';
+import { ThemeProvider } from '@emotion/react';
+import theme from '../helpers/theme';
 
 
 const App = () => {
     return (
         <main>
-            <RouterProvider router={router} />
-            <h1 className="text-3xl font-bold underline">
+            <ThemeProvider theme={theme}>
+                <RouterProvider router={router} />
+                <h1 className="text-3xl font-bold underline">
                 Tailwind css working
-            </h1>
+                </h1>
+            </ThemeProvider>
         </main>
     );
 }
---
+++ client/src/helpers/theme.js
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles';
+import { pink, green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        fontSize: [
+            500, 700
+        ]
+    },
+    palette: {
+        primary: {
+            light: '#42a5f5',
+            main: '#1976d2',
+            dark: '#1565c0',
+
+        },
+        secondary: {
+            main: green[900]
+        },
+        error: {
+            main: '#d32f2f',
+            light: '#ef5350',
+            dark: '#c62828'
+        },
+        warning: {
+            main: '#ed6c02',
+            light: '#ff9800',
+            dark: '#e65100'
+        },
+        info: {
+            main: '#0288d1',
+            light: '#03a9f4',
+            dark: '#01579b'
+        },
+        success: {
+            light: '#4caf50',
+            main: '#2e7d32',
+            dark: '#1b5e20'
+        }
+    }
+});
+
+export default theme;
